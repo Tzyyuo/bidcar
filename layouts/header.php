2b501aa5647548564da265dba3e52dfbca3eb153
azvlyy <aryasatyamahardika658@gmail.com>
2025-05-14 14:27:44 +0700

    horhorhorhor

--- layouts/header.php
+++ layouts/header.php
@@ -1,6 +1,15 @@
-<navbar class="navigasi">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="/bidcar/css/header.css">
+</head>
+<body>
+    <navbar class="navigasi">
     <div class="logo">
-        <img src="bidcar.png">
+        <img src="/bidcar/img/bidcar alt color 1.png">
         <ul>
         <li><a href="/bidcar/model/masyarakat/index.php">Home</a></li>
         <li><a href="/bidcar/layouts/tentang.php">Tentang</a></li>
@@ -8,8 +17,16 @@
         </ul>
     </div>
     <div class="button">
-        <button class = "btn-masuk" type="menu"><a href=""-> Masuk</button>
-        <button class = "btn-kontak-kami" type="menu">Kontak Kami</button>
+        <button class="btn-masuk">
+        <img src="/bidcar/img/Login icon.png" alt="Login Icon" class="icon"> Masuk
+        </button>
+
+        <button class="btn-kontak-kami">
+         Kontak Kami <img src="/bidcar/img/phone icon.png" alt="Phone Icon" class="icon">
+        </button>
     </div>
 </navbar>
+</body>
+</html>
+
 
---
+++ model/masyarakat/indexcopy.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard - BidCar.</title>
+</head>
+<body>
+    <?php include '../../layouts/header.php';?>
+    <style>
+    <?php include '../../css/header.css'; ?>
+    </style>
+
+    <!-- Bagian Sebelum Registrasi -->
+    <?php include '../../layouts/banner.php';?>
+    <style>
+    <?php include '../../css/banner.css'; ?>
+    </style>
+
+    <?php include '../../layouts/tentang.php';?>
+    <?php include '../../layouts/pusatbantuan.php';?>
+    <?php include '../../layouts/footer.php';?>
+</body>
+</html>
+
